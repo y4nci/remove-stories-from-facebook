da683aecf309e07596e28d5c20dcdadf99c739c3
Baran Yancı <73401941+y4nci@users.noreply.github.com>
2023-11-09 08:52:58 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Remove Stories from Facebook
+A simple Chrome extension to remove stories from Facebook.
+
+Are you bored of seeing stories on Facebook? You opened Facebook to see what your friends are up to, but the first thing you (and the people next to you) see is a story from a page you don't even remember following? Well, this extension is for you.
+
+If you have any issues or something's broken, you can open an issue [here](https://github.com/y4nci/remove-stories-from-facebook/issues).
+
+## Reporting Issues
+ - If you find any issues, please report them [here](https://github.com/y4nci/remove-stories-from-facebook/issues).
+
+## Collaborating
+ - If you want to collaborate, feel free to fork this repository and create a pull request. You can also email me [here](mailto:baranyanci@gmail.com).
---
+++ background.js
@@ -0,0 +1,56 @@
+let tabID = null;
+
+
+const main = () => {
+    /**
+     * thanks to Yong Wang for their answer on StackOverflow
+     * https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
+     */
+    const waitForComponent = (selector) => {
+        return new Promise(resolve => {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
+            }
+    
+            const observer = new MutationObserver(mutations => {
+                if (document.querySelector(selector)) {
+                    resolve(document.querySelector(selector));
+                    observer.disconnect();
+                }
+            });
+    
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        });
+    };
+
+    waitForComponent('div[role="main"]>div>div>div').then(elem => {
+        const mainComponent = elem;
+        const storiesComponent = mainComponent.children[1];
+
+        storiesComponent.remove();
+    });
+};
+
+const isFacebookUrl = (url) => {
+    const regex = /https:\/\/facebook\.com\/.*/;
+    return regex.test(url);
+};
+
+const injectScript = (func=main) => chrome.scripting.executeScript({
+    target: {
+        tabId: tabID,
+        allFrames: true
+    },
+    function: func
+});
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    tabID = tabId;
+
+    if (isFacebookUrl(tab.url) && changeInfo.status === 'complete') {
+        await injectScript(main);
+    }
+});
